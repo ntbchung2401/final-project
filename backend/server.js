c43360eee872b71b8708df6e518f1f9da5503eb2
ntbchung2401 <85856695+ntbchung2401@users.noreply.github.com>
2023-02-25 02:49:53 +0700

    Apply Bootstrap and Update Product Details Screen

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import data from "./data.js";
+//test
 const app = express();
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`server at http://localhost:${port}`);
+  console.log(`serve at http://localhost:${port}`);
 });
